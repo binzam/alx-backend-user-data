aa3fa2925de27148564341b0dec606d45e337928
root <btechan@gmail.com>
2024-05-15 21:39:53 +0000

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    patterns = {
+        "extract": lambda x, y: r"(?P<field>{})=[^{}]*".format("|".join(x), y),
+        "replace": lambda x: r"\g<field>={}".format(x),
+    }
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
