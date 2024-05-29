9d18ff1bf8bc2fc4263f20b8f51a0c9e84bb755d
binzam <btechan@gmail.com>
2024-05-29 03:45:56 +0300

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,9 +84,3 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
-
-        #     reset_token = _generate_uuid()
-        #     self._db.update_user(email, reset_token=reset_token)
-        #     return reset_token
-        # except NoResultFound:
-        #     raise ValueError
