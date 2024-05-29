76551481d7d5c17cb2bc569058988533a3e1ffd7
binzam <btechan@gmail.com>
2024-05-30 00:12:44 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,7 +83,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"])
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password():
     """Update user password"""
     email = request.form.get("email")
