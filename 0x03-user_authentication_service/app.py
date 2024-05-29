6b7ae3f682ff41e2b9891c1483825b48f8914392
binzam <btechan@gmail.com>
2024-05-30 00:10:58 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,8 +93,7 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
-        pass
-    abort(403)
+        abort(403)
 
 
 if __name__ == "__main__":
