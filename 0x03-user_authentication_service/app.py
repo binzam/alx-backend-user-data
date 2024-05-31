97537335ee0ae9ca2b7ba2eefb88ef815b9d9967
binoyam <btechan@gmail.com>
2024-05-31 04:24:43 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,7 +81,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"])
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """Update user password"""
     email = request.form.get("email")
@@ -92,8 +92,7 @@ def update_password() -> str:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
-    response = {"email": f"{email}", "message": "Password updated"}
-    return jsonify(response)
+    return jsonify({"email": f"{email}", "message": "Password updated"})
 
 
 if __name__ == "__main__":
