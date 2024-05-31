764792702f354c15de78cb919f442f47dcf9183e
binoyam <btechan@gmail.com>
2024-05-31 04:47:13 +0300

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,10 +11,7 @@ AUTH = Auth()
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
-    """
-    Return json respomse
-    {"message": "Bienvenue"}
-    """
+    """Return  {"message": "Bienvenue"}"""
     return jsonify({"message": "Bienvenue"})
 
 
